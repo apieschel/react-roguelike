7d7709eefd73934d9825faf54ad9e56455fbefc5
Glitch (create-react-app-sample) <none>
2019-01-13 21:16:52 +0000

    👫🌷 Checkpoint ./src/App.js:2265073/2312 ./src/App.css:2265073/4

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+      
+		}
+		this.constructMap = this.constructMap.bind(this);
+	}
+  
+  constructMap() {
+    let grid = [];
+	  for(let i = 0; i < 50; i++) {
+      grid.push(<div className="gridItem"></div>);
+    }
+    return grid;
+  }
+  
   render() {
     return (
       <div className="App">
         <div className="map">
           <div className="fieldOfVision">
-            <Game/>
+            {this.constructMap()}
           </div>
         </div>
       </div>
     );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    for(let i = 0; i < 50; i++) {
-      return(
-        <div className="grid">
-        </div>
-      );
-    } 
   }
 }
 
