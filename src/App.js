7bc831dbd62c4888ed01adeaeee481a42e365ba0
Glitch (create-react-app-sample) <none>
2019-01-13 22:00:05 +0000

    🍏🐊 Checkpoint ./src/App.js:2265073/466 ./src/App.css:2265073/34

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends Component {
   constructor(props) {
 		super(props);
 		this.state = {
-      userPosition: 67
+      userPosition: 67,
+      userHealth: 100,
+      userLevel: 0,
+      userWeapon: "Sword"
 		}
 		this.constructMap = this.constructMap.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -56,7 +59,7 @@ class App extends Component {
     let j = this.state.userPosition;
 	  for(let i = 0; i < 1500; i++) {
       if(i === j) {
-        grid.push(<div className="user" key={i}></div>);
+        grid.push(<div className="user" key={i}>(:</div>);
       } else {
         grid.push(<div className="gridItem" key={i}></div>);
       }
@@ -67,11 +70,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="map">
-          <div className="fieldOfVision">
-            {this.constructMap()}
+        <div className="container">
+          <div className="map">
+            <div className="fieldOfVision">
+              {this.constructMap()}
+            </div>
           </div>
         </div>
+        <p>Health: {this.state.userHealth}</p>
+        <p>Level: {this.state.userLevel}</p>
+        <p>Weapon: {this.state.userWeapon}</p>
       </div>
     );
   }
